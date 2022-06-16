225bda0cd75223f1584020fc27e814bd0ce19606
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-06-16 17:40:57 +0300

    btn

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import BasicCard from './BasicCard';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <BasicCard />
+        <Button variant="contained">Hello World</Button>
+        <Button color="primary">Hello World</Button>
         <Counter />
       </header>
     </div>
