2c76e27242943f0ee51852faf620ed7ced145c53
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-07-21 10:43:00 +0300

    hooks

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,26 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from './store';
+import {useEffect, useRef} from "react";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export function usePrevious<T>(value: T) {
+    const ref = useRef<T>()
+    useEffect(() => {
+        ref.current = value
+    }, [value])
+    return ref.current
+}
+
+export function useChange<T>(value: T, f: Function, log?: Function) {
+    const dispatch = useAppDispatch()
+    const prev_value = usePrevious(value)
+    useEffect(() => {
+        log && dispatch(log(`Change: ${prev_value} -> ${value}`))
+        if (null != prev_value) {
+            dispatch(f([prev_value, value]))
+        }
+    }, [value])
+}
--- src/features/gametable/GameTable.tsx
+++ src/features/gametable/GameTable.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {useAppSelector, useAppDispatch} from "../../app/hooks"
+import {useAppSelector, useAppDispatch, useChange} from "../../app/hooks"
 
 import {gameStyles} from "./gameStyles"
 import {gameState, change_gt, change_pt, change_ph, log, next} from "./gameTableSlice"
@@ -15,47 +15,11 @@ function GameTable() {
     const state = useAppSelector(gameState)
     const dispatch = useAppDispatch()
 
-    // Hook
-    function usePrevious<T>(value: T) {
-        const ref = useRef<T>()
-        useEffect(() => {
-            ref.current = value
-        }, [value])
-        return ref.current
-    }
-
-    //React Hooks
-    const prev_ph = usePrevious(state.cur_ph)
-    useEffect(() => {
-        // dispatch(log(`Ph: ${prev_ph} -> ${state.cur_ph}`))
-        if (null != prev_ph) {
-            dispatch(change_ph([prev_ph, state.cur_ph]))
-        }
-    }, [state.cur_ph])
-
-    const prev_pt = usePrevious(state.cur_pt)
-    useEffect(() => {
-        // dispatch(log(`PT: ${prev_pt} -> ${state.cur_pt}`))
-        if (null != prev_pt) {
-            dispatch(change_pt([prev_pt, state.cur_pt]))
-        }
-    }, [state.cur_pt])
-
-    const prev_gt = usePrevious(state.cur_gt)
-    useEffect(() => {
-        // dispatch(log(`GT: ${prev_gt} -> ${state.cur_gt}`))
-        if (null != prev_gt) {
-            dispatch(change_gt([prev_gt, state.cur_gt]))
-        }
-    }, [state.cur_gt])
-
-    const prev_cnt = usePrevious(state.cur_gt)
-    useEffect(() => {
-        // dispatch(log(`CNT: ${prev_cnt} -> ${state.next_cnt}`))
-        if (null != prev_cnt) {
-            dispatch(next())
-        }
-    }, [state.next_cnt])
+    //Hooks
+    useChange(state.cur_ph, change_ph)
+    useChange(state.cur_pt, change_pt)
+    useChange(state.cur_gt, change_gt)
+    useChange(state.next_cnt, next)
 
     const classes = gameStyles()
 
