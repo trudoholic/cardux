84de322dc833090b1056641404625a8e350725f3
Oleg <35769958+trudoholic@users.noreply.github.com>
2022-08-11 09:18:40 +0300

    zone enum

--- src/features/gametable/config.ts
+++ src/features/gametable/config.ts
@@ -1,9 +1,21 @@
+export enum CommonZone {
+    play,
+    deck,
+    drop,
+}
+
+export enum PlayerZone {
+    hand,
+    keep,
+}
+
+export const enumToArray = (e:object) => Object.keys(e).filter(k => isNaN(+k))
+
 const config = {
     foo: 'bar',
     players: ['North','East','South','West'],
-    player_zones: ['hand','keep'],
-    common_zones: ['play','deck','drop'],
-    // phases: ['draw','play','spam','ham','eggs'],
+    player_zones: enumToArray(PlayerZone),
+    common_zones: enumToArray(CommonZone),
     phases: ['draw','play','hl','kl'],
     ph_lim: [2,1,1,1],
     phase_zone: ['','hand','hand','keep']
